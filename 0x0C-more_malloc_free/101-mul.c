67104ccbdd311a662f13a9bb7e9e5a001a572925
Brenda-17 <bhrehndar17@gmail.com>
2022-04-07 19:39:53 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,46 +4,29 @@
 #include <math.h>
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
-int is_digit(char *s)
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	unsigned long mul;
+	int i, j;
+		if (argc != 3)
+		{ printf("Error\n");
+		exit(98); }
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] > 57 || argv[i][j] < 48)
+				{  printf("Error\n");
+				exit(98); }
+			}
 
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * errors - handles errors for main
- */
-void errors(void)
-{
-	print("Error\n");
-	exit(98);
+		}
+		mul = atol(argv[1]) *  atol(argv[2]);
+		printf("%lu\n", mul);
+		return (0);
 }
